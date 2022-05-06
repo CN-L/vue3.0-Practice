a882ea05ea8dd4ae9cb00f9a7a2b47ec2621ecb5
CHINA-L <15122163669@163.com>
2022-05-06 23:08:13 +0800

    feat($add): 新增watch监听基础数据类型和复杂数据类型中的对象的用法

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <!-- 在模版对面它会将ref对象直接把值展示出来，所以不需要写.value -->
     {{count}}<span @click="increase">👍</span>
     <div>{{double}}</div>
+    <button @click="updaetGreenying">up title</button>
     <ul>
       <li v-for="number in numbers" :key="number">{{number}}</li>
     </ul>
@@ -13,7 +14,7 @@
   <router-view/>
 </template>
 <script lang="ts">
-import { defineComponent, ref, computed, reactive, toRefs } from 'vue'
+import { defineComponent, ref, computed, reactive, toRefs, onMounted, onUpdated, onRenderTriggered, watch } from 'vue'
 interface DataProps {
   count: number,
   double: number,
@@ -25,6 +26,15 @@ interface DataProps {
 }
 export default defineComponent({
   setup () {
+    onMounted(() => {
+      console.log('mouted')
+    })
+    onUpdated(() => {
+      console.log('updated')
+    })
+    onRenderTriggered((event) => {
+      console.log('onRenderTriggered', event)
+    })
     // ref 一般用于原始类型
     const conut = ref(0)
     // 一般用于复杂类型
@@ -39,15 +49,26 @@ export default defineComponent({
     const changeCount = () => {
       conut.value++
     }
+    const greeting = ref('')
+    const updaetGreenying = () => {
+      greeting.value = 'hellow' + form.count
+    }
+    // 监听
+    watch([greeting, () => form.count], (newVal, oldVal) => {
+      console.log(newVal, oldVal)
+      document.title = greeting.value
+    })
     form.numbers[0] = 10
     form.person.name = '我是你爹'
     // form.personw.age = 90
     const double = computed(() => {
       return conut.value * 2
     })
+
     const toRefData = toRefs(form)// 解构后丧失响应性 ，toRefs可保持响应式
     return {
-      ...toRefData
+      ...toRefData,
+      updaetGreenying
       // conut,
       // double,
       // changeCount
