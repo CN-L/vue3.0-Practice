9fc1f700ab92131fb0c7e40603d6c25b86c8948d
CHINA-L <15122163669@163.com>
2022-05-06 14:35:13 +0800

    feat($add): vue3.0可以直接更改数组或者对象 让vue.set成了过去时

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,11 @@
   <!-- <nav> -->
     <!-- 在模版对面它会将ref对象直接把值展示出来，所以不需要写.value -->
     {{count}}<span @click="increase">👍</span>
-    {{double}}
+    <div>{{double}}</div>
+    <ul>
+      <li v-for="number in numbers" :key="number">{{number}}</li>
+    </ul>
+    <h1>{{person.name}}</h1>
     <!-- <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> -->
   <!-- </nav> -->
@@ -13,6 +17,10 @@ import { defineComponent, ref, computed, reactive, toRefs } from 'vue'
 interface DataProps {
   count: number,
   double: number,
+  numbers: number[],
+  person: {
+    name?: string
+  }
   increase: ()=> void
 }
 export default defineComponent({
@@ -23,16 +31,21 @@ export default defineComponent({
     const form: DataProps = reactive({
       count: 0,
       increase: () => { form.count++ },
-      double: computed(() => form.count * 2)
+      double: computed(() => form.count * 2),
+      numbers: [0, 1],
+      person: {}
     })
     // 使用计算属性
     const changeCount = () => {
       conut.value++
     }
+    form.numbers[0] = 10
+    form.person.name = '我是你爹'
+    // form.personw.age = 90
     const double = computed(() => {
       return conut.value * 2
     })
-    const toRefData = toRefs(form)//解构后丧失响应性 ，toRefs可保持响应式
+    const toRefData = toRefs(form)// 解构后丧失响应性 ，toRefs可保持响应式
     return {
       ...toRefData
       // conut,
