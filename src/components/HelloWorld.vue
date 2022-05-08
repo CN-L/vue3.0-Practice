f1d90a81d861c381c43034d3734d146d7963c31d
CHINA-L <15122163669@163.com>
2022-05-08 14:04:00 +0800

    feat($teleport): teleport的妙用 可以直接渲染在指定位置

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="hello">
+    dajiahao
     <h1>{{ msg }}</h1>
+    <my-modal></my-modal>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue' // defineComponent完全为了服务ts所存在
-
+import myModal from '@/components/Modal.vue'
 export default defineComponent({
   name: 'HelloWorld',
   props: {
@@ -14,6 +16,9 @@ export default defineComponent({
       required: true,
       type: String
     }
+  },
+  components: {
+    myModal
   },
   setup (props, context) {
     // props.msg
---
+++ src/components/Modal.vue
@@ -0,0 +1,31 @@
+<template>
+<teleport to="#modal">
+  <div class="modal1">
+    <div>大家好</div>
+  </div>
+</teleport>
+
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'myModal',
+  setup () {
+    return {
+
+    }
+  }
+})
+</script>
+<style lang="scss">
+.modal1 {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  height: 100px;
+  width: 100px;
+  background: yellow;
+}
+</style>
