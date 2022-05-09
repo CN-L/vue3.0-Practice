a661567dcab771865ff6a08d9c60f4f2b9a44c0e
CHINA-L <15122163669@163.com>
2022-05-09 17:10:24 +0800

    feat($setup进阶): 接收参数且校验类型defineProps 和 触发父组件方法defineEmits和校验类型

--- src/App-2.vue
+++ src/App-2.vue
@@ -2,9 +2,18 @@
   <div>{{hello9}}</div>
   <button @click="hello9++">add</button>
   <HelloWord msg="巴嘎"></HelloWord>
+  <TestMsg @change="showAloer" :user='user'></TestMsg>
 </template>
 <script setup lang="ts">
 import { ref } from 'vue'
 import HelloWord from '@/components/HelloWorld.vue'
-const hello9 = ref(0)
+import TestMsg from '@/components/test-msg.vue'
+const hello9 = ref<number>(0)
+const user = {
+  name: 'viking',
+  age: 90
+}
+const showAloer = (txt: number) => {
+  console.log(txt)
+}
 </script>
---
+++ src/components/test-msg.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="msg">
+    {{user.name}}
+  </div>
+  <button @click="onChange">发送</button>
+</template>
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue'
+interface Muser {
+  name: string,
+  age: number
+}
+interface Ievent {
+  (e: 'change', age: number): void
+}
+const props = defineProps<{user: Muser}>()
+const emit = defineEmits<Ievent>()
+const onChange = () => {
+  emit('change', props.user.age)
+}
+</script>
+<style scoped>
+
+</style>
