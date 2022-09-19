358eddcd25761759b47979d4eaefb658e2d8edfe
CN-L <41163551+CN-L@users.noreply.github.com>
2022-09-19 19:10:46 +0800

    feat($代理): 关于前端代理、本地localhost代理到cat-api 和 dog-api
    虽然明面上走的还是本地localhost  但是实际上拼接会代理到后台地址

--- src/App-2.vue
+++ src/App-2.vue
@@ -1,12 +1,13 @@
 <template>
   <div>{{hello9}}</div>
   <button @click="hello9++">add</button>
-  <HelloWord msg="巴嘎"></HelloWord>
+  <HelloWord msg="1"></HelloWord>
   <test-msg @change="showAloer" :user='user'></test-msg>
   <h1 v-if="keyIsDwon">h被按下了哦</h1>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
 import HelloWord from '@/components/HelloWorld.vue'
 import TestMsg from '@/components/test-msg.vue'
 import useKeyHandle from '@/hooks/useKeyUp'
@@ -16,6 +17,14 @@ const user = {
   name: 'viking',
   age: 90
 }
+onMounted(() => {
+  axios.get('/dog/api/breeds/image/random').then(res => {
+    console.log(res.data, '狗的照片')
+  })
+  axios.get('/cat/api/images/search').then(res => {
+    console.log(res.data, '猫的照片')
+  })
+})
 const showAloer = (txt: number) => {
   console.log(txt)
 }
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,27 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    // host: 'localhost',
+    port: '8888',
+    https: true,
+    proxy: {
+      '/dog/api': {
+        target: 'https://dog.ceo',
+        ws: true,
+        changeOrigin: true,
+        pathRewrite: {
+            '^/dog/api': '/api'
+        }
+      },
+      '/cat/api': {
+        target: 'https://api.thecatapi.com',
+        ws: true,
+        changeOrigin: true,
+        pathRewrite: {
+            '^/cat/api': '/v1'
+        }
+      }
+    },
+  }
 })
